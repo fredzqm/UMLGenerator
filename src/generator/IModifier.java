747f1a704bf455ebe7c2178922816454c4d39aae
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-12-10 18:30:23 -0500

    adjust the variable declarations of generators

--- src/generator/IModifier.java
+++ src/generator/IModifier.java
@@ -3,15 +3,16 @@ package generator;
 public interface IModifier {
 	String getModifierSymbol();
 	
-//	void switchByCase(Switcher switcher);
-//	public interface Switcher {
-//		void ifPrivate();
-//
-//		void ifPublic();
-//
-//		void ifProtected();
-//
-//		void ifDefault();
-//	}
+	void switchByCase(Switcher switcher);
+
+	public interface Switcher {
+		void ifPrivate();
+
+		void ifPublic();
+
+		void ifProtected();
+
+		void ifDefault();
+	}
 
 }
