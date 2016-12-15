26ce65a34463f9ab487fb27d21fc721261b331de
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-12-15 11:03:39 -0500

    combine ASMParser and NonRecursiveASMParser, use a final flag to indicate what kind of parser it is

---
+++ src/main/java/model/ASMClassTracker.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * The concrete ASM service provider that will recursively parse all related
+ * classes request. {@see NonRecursiveASMParser}
+ * 
+ * @author zhang
+ *
+ */
+public interface ASMClassTracker extends ASMServiceProvider {
+
+	/**
+	 * add those classes to ASMClassTracker by explicitly
+	 * 
+	 * @param importClassesList
+	 */
+	void addClasses(Iterable<String> importClassesList);
+
+	/**
+	 * 
+	 * @return an iterable of all the classes this ASM Class Tracker currently
+	 *         contains
+	 */
+	public Iterable<ClassModel> getClasses();
+}
