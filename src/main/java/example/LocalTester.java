23d343f03e2bbefc79e327236776039a23ea2e72
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-12-16 18:12:27 -0500

    refactor IFilter anoymouse class to use java8 syntax

--- src/main/java/example/LocalTester.java
+++ src/main/java/example/LocalTester.java
@@ -19,63 +19,59 @@ import java.util.List;
  * Created by lamd on 12/15/2016.
  */
 public class LocalTester {
-    public static void main(String[] args) {
-        localTester();
-    }
+	public static void main(String[] args) {
+		localTester();
+	}
 
-    private static ISystemModel setupSystemModel() {
-        Configuration config = Configuration.getInstance();
-        List<String> classList = new ArrayList<>();
-        classList.add(GraphVizGenerator.class.getPackage().getName() + "." + GraphVizGenerator.class.getSimpleName());
-        classList.add("java.lang.String");
-        config.setClasses(classList);
-        config.setRecursive(true);
-        return SystemModel.getInstance(config);
-    }
+	private static ISystemModel setupSystemModel() {
+		Configuration config = Configuration.getInstance();
+		List<String> classList = new ArrayList<>();
+		classList.add(GraphVizGenerator.class.getPackage().getName() + "." + GraphVizGenerator.class.getSimpleName());
+		classList.add("java.lang.String");
+		config.setClasses(classList);
+		config.setRecursive(true);
+		return SystemModel.getInstance(config);
+	}
 
-    private static void localTester() {
-        // Set up the system model and config.
-        ISystemModel systemModel = setupSystemModel();
+	private static void localTester() {
+		// Set up the system model and config.
+		ISystemModel systemModel = setupSystemModel();
 
-        // Set up config.
-        Configuration config = Configuration.getInstance();
-        config.setFilters(new IFilter<Modifier>() {
-            @Override
-            public boolean filter(Modifier data) {
-                return data == Modifier.DEFAULT || data == Modifier.PUBLIC;
-            }
+		// Set up config.
+		Configuration config = Configuration.getInstance();
+		config.setFilters((d) -> d == Modifier.DEFAULT || d == Modifier.PUBLIC);
+		config.setNodesep(1.0);
+		config.setRecursive(true);
+		config.setRankDir("BT");
+		config.setOutputDirectory("./output");
+		config.setFileName("testFilter");
+		config.setExecutablePath("dot");
 
-        });
-        config.setNodesep(1.0);
-        config.setRecursive(true);
-        config.setRankDir("BT");
-        config.setOutputDirectory("./output");
-        config.setFileName("testFilter");
-        config.setExecutablePath("dot");
+		// Create GraphVizGenerator.
+		IGenerator generator = new GraphVizGenerator(config);
 
-        // Create GraphVizGenerator.
-        IGenerator generator = new GraphVizGenerator(config);
+		String actual = generator.generate(systemModel, null);
 
-        String actual = generator.generate(systemModel, null);
+		internalRunner(config, actual); // Comment out if you want actual files
+		// to be
+	}
 
-        internalRunner(config, actual); // Comment out if you want actual files
-        // to be
-    }
-
-    /**
-     * Interal Testing Runner method to call for actual output.
-     *
-     * @param config         Configuration for the runner to use.
-     * @param graphVizString GraphViz DOT string t
-     */
-    private static void internalRunner(Configuration config, String graphVizString) {
-        // Create the runner
-        IRunner runner = new GraphVizRunner();
-        try {
-            runner.execute(config, graphVizString);
-        } catch (Exception e) {
-            System.err.println("[ INFO ]: Ensure that GraphViz bin folder is set in the environment variable.");
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Interal Testing Runner method to call for actual output.
+	 *
+	 * @param config
+	 *            Configuration for the runner to use.
+	 * @param graphVizString
+	 *            GraphViz DOT string t
+	 */
+	private static void internalRunner(Configuration config, String graphVizString) {
+		// Create the runner
+		IRunner runner = new GraphVizRunner();
+		try {
+			runner.execute(config, graphVizString);
+		} catch (Exception e) {
+			System.err.println("[ INFO ]: Ensure that GraphViz bin folder is set in the environment variable.");
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/model/ClassModelTest.java
+++ src/test/java/model/ClassModelTest.java
@@ -4,6 +4,7 @@ import labTestCI.AmazonLineParser;
 import labTestCI.ILineParser;
 import org.junit.Test;
 import utility.IFilter;
+import utility.MethodType;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -13,104 +14,98 @@ import static org.junit.Assert.assertEquals;
 
 public class ClassModelTest {
 
-    @Test
-    public void testGetField() {
-        ASMServiceProvider parser = new ASMParser();
-        ClassModel model = parser.getClassByName("java.lang.String");
-        assertEquals("java.lang.String", model.getName());
+	@Test
+	public void testGetField() {
+		ASMServiceProvider parser = new ASMParser();
+		ClassModel model = parser.getClassByName("java.lang.String");
+		assertEquals("java.lang.String", model.getName());
 
-        Set<String> fields = new HashSet<>();
-        Set<String> actfields = new HashSet<>(
-                Arrays.asList("value", "hash", "serialVersionUID", "serialPersistentFields", "CASE_INSENSITIVE_ORDER"));
+		Set<String> fields = new HashSet<>();
+		Set<String> actfields = new HashSet<>(
+				Arrays.asList("value", "hash", "serialVersionUID", "serialPersistentFields", "CASE_INSENSITIVE_ORDER"));
 
-        model.getFields().forEach((field) -> fields.add(field.getName()));
+		model.getFields().forEach((field) -> fields.add(field.getName()));
 
-        assertEquals(actfields, fields);
-    }
+		assertEquals(actfields, fields);
+	}
 
-    @Test
-    public void testGetMethods() {
-        ASMServiceProvider parser = new ASMParser();
-        ClassModel model = parser.getClassByName("model.Dummy");
+	@Test
+	public void testGetMethods() {
+		ASMServiceProvider parser = new ASMParser();
+		ClassModel model = parser.getClassByName("model.Dummy");
 
-        Set<String> actual = new HashSet<>();
-        Set<String> expected = new HashSet<>(Arrays.asList("publicMethod", "publicMethod2", "privateMethod"));
+		Set<String> actual = new HashSet<>();
+		Set<String> expected = new HashSet<>(Arrays.asList("publicMethod", "publicMethod2", "privateMethod"));
 
-        IFilter<MethodModel> getInstanceMethod = (data) -> {
-            switch (data.getMethodType()) {
-                case METHOD:
-                    return true;
-                default:
-                    return false;
-            }
-        };
+		IFilter<MethodModel> getInstanceMethod = (d) -> d.getMethodType() == MethodType.METHOD;
 
-        getInstanceMethod.filter(model.getMethods()).forEach(methodModel -> actual.add(methodModel.getName()));
+		getInstanceMethod.filter(model.getMethods()).forEach(methodModel -> actual.add(methodModel.getName()));
 
-        assertEquals(expected, actual);
-    }
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void testGetInterface() {
-        ASMServiceProvider parser = new ASMParser();
-        ClassModel model = parser.getClassByName("java.lang.String");
-        assertEquals("java.lang.String", model.getName());
+	@Test
+	public void testGetInterface() {
+		ASMServiceProvider parser = new ASMParser();
+		ClassModel model = parser.getClassByName("java.lang.String");
+		assertEquals("java.lang.String", model.getName());
 
-        Set<String> acutalInterfaces = new HashSet<>();
-        Set<String> expectInterfaces = new HashSet<>();
+		Set<String> acutalInterfaces = new HashSet<>();
+		Set<String> expectInterfaces = new HashSet<>();
 
-        expectInterfaces.add("java.io.Serializable");
-        expectInterfaces.add("java.lang.Comparable");
-        expectInterfaces.add("java.lang.CharSequence");
+		expectInterfaces.add("java.io.Serializable");
+		expectInterfaces.add("java.lang.Comparable");
+		expectInterfaces.add("java.lang.CharSequence");
 
-        model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
+		model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
 
-        assertEquals(expectInterfaces, acutalInterfaces);
-    }
+		assertEquals(expectInterfaces, acutalInterfaces);
+	}
 
-    @Test
-    public void testGetStringInterfaceNonRecursive() {
-        ASMClassTracker parser = ASMParser.getInstance(new IModelConfiguration() {
-            @Override
-            public boolean isRecursive() {
-                return false;
-            }
+	@Test
+	public void testGetStringInterfaceNonRecursive() {
+		ASMClassTracker parser = ASMParser.getInstance(new IModelConfiguration() {
+			@Override
+			public boolean isRecursive() {
+				return false;
+			}
 
-            @Override
-            public Iterable<String> getClasses() {
-                return Arrays.asList("java.lang.String", "java/io/Serializable", "java/lang/Comparable");
-            }
-        });
-        ClassModel model = parser.getClassByName("java/lang/String");
-        assertEquals("java.lang.String", model.getName());
+			@Override
+			public Iterable<String> getClasses() {
+				return Arrays.asList("java.lang.String", "java/io/Serializable", "java/lang/Comparable");
+			}
+		});
+		ClassModel model = parser.getClassByName("java/lang/String");
+		assertEquals("java.lang.String", model.getName());
 
-        Set<String> acutalInterfaces = new HashSet<>();
-        Set<String> expectInterfaces = new HashSet<>();
+		Set<String> acutalInterfaces = new HashSet<>();
+		Set<String> expectInterfaces = new HashSet<>();
 
-        expectInterfaces.add("java.io.Serializable");
-        expectInterfaces.add("java.lang.Comparable");
+		expectInterfaces.add("java.io.Serializable");
+		expectInterfaces.add("java.lang.Comparable");
 
-        model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
+		model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
 
-        assertEquals(expectInterfaces, acutalInterfaces);
-    }
+		assertEquals(expectInterfaces, acutalInterfaces);
+	}
 
-    @Test
-    public void testGetInterfaceLab_1_AmazonParser() {
-        ASMServiceProvider parser = new ASMParser();
-        String amazonQualifiedString = AmazonLineParser.class.getPackage().getName() + "." + AmazonLineParser.class.getSimpleName();
-        ClassModel model = parser.getClassByName(amazonQualifiedString);
-        assertEquals(amazonQualifiedString, model.getName());
+	@Test
+	public void testGetInterfaceLab_1_AmazonParser() {
+		ASMServiceProvider parser = new ASMParser();
+		String amazonQualifiedString = AmazonLineParser.class.getPackage().getName() + "."
+				+ AmazonLineParser.class.getSimpleName();
+		ClassModel model = parser.getClassByName(amazonQualifiedString);
+		assertEquals(amazonQualifiedString, model.getName());
 
-        Set<String> acutalInterfaces = new HashSet<>();
-        Set<String> expectInterfaces = new HashSet<>();
+		Set<String> acutalInterfaces = new HashSet<>();
+		Set<String> expectInterfaces = new HashSet<>();
 
-        String expected = ILineParser.class.getPackage().getName() + "." + ILineParser.class.getSimpleName();
-        expectInterfaces.add(expected);
+		String expected = ILineParser.class.getPackage().getName() + "." + ILineParser.class.getSimpleName();
+		expectInterfaces.add(expected);
 
-        model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
+		model.getInterfaces().forEach((interfaceModel) -> acutalInterfaces.add(interfaceModel.getName()));
 
-        assertEquals(expectInterfaces, acutalInterfaces);
-    }
+		assertEquals(expectInterfaces, acutalInterfaces);
+	}
 
 }
