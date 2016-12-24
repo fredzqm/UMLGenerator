1b4becab3f530266a6a8120040005d5ef59a3216
finercoder <fineral@rose-hulman.edu>
2016-12-23 22:51:57 -0500

    added extra safeguards for the json file parser

--- src/main/java/config/ConfigFileParser.java
+++ src/main/java/config/ConfigFileParser.java
@@ -29,53 +29,71 @@ public class ConfigFileParser implements ConfigurationFactory {
 	public Configuration create() throws Exception {
 		Configuration conf = Configuration.getInstance();
 		JSONObject jsonConf = json.getJSONObject("config");
-		JSONArray classes = jsonConf.getJSONArray("classes");
+		if (jsonConf.has("classes")){
+			JSONArray classes = jsonConf.getJSONArray("classes");
+	
+			List<String> listClasses = new ArrayList<>();
+			for (int i = 0; i < classes.length(); i++)
+				listClasses.add(classes.getString(i));
+			conf.setClasses(listClasses);
+		}
+		
+		if (jsonConf.has("executablePath"))
+			conf.setExecutablePath(jsonConf.getString("executablePath"));
 
-		List<String> listClasses = new ArrayList<>();
-		for (int i = 0; i < classes.length(); i++)
-			listClasses.add(classes.getString(i));
-		conf.setClasses(listClasses);
+		if (jsonConf.has("outputFormat"))
+			conf.setOutputFormat(jsonConf.getString("outputFormat"));
 
-		conf.setExecutablePath(jsonConf.getString("executablePath"));
+		if (jsonConf.has("outputDir"))
+			conf.setOutputDirectory(jsonConf.getString("outputDir"));
 
-		conf.setOutputFormat(jsonConf.getString("outputFormat"));
+		if (jsonConf.has("fileName"))
+			conf.setFileName(jsonConf.getString("fileName"));
 
-		conf.setOutputDirectory(jsonConf.getString("outputDir"));
-
-		conf.setFileName(jsonConf.getString("fileName"));
-
+		if (jsonConf.has("nodeSep"))
 		conf.setNodesep(jsonConf.getDouble("nodeSep"));
 
-		String filter = jsonConf.getString("modifierFilter");
-
-		List<Modifier> filters = new ArrayList<>();
-		switch (filter) {
-		case ("public"):
-			filters.add(Modifier.PRIVATE);
-			filters.add(Modifier.PROTECTED);
-			break;
-		case ("protected"):
-			filters.add(Modifier.PRIVATE);
-			break;
-		case ("private"):
-			break;
-		default:
-			System.err.println("modifier not found");
+		if (jsonConf.has("modifierFilter")){
+			String filter = jsonConf.getString("modifierFilter");
+	
+			List<Modifier> filters = new ArrayList<>();
+			switch (filter) {
+			case ("public"):
+				filters.add(Modifier.PRIVATE);
+				filters.add(Modifier.PROTECTED);
+				break;
+			case ("protected"):
+				filters.add(Modifier.PRIVATE);
+				break;
+			case ("private"):
+				break;
+			default:
+				System.err.println("modifier not found");
+			}
+			conf.setFilters(new IFilter<Modifier>() {
+				@Override
+				public boolean filter(Modifier data) {
+					return !filters.contains(data);
+				}
+				
+				public String toString() {
+					return filters.toString();
+				}
+			});
 		}
-		conf.setFilters(new IFilter<Modifier>() {
-			@Override
-			public boolean filter(Modifier data) {
-				return !filters.contains(data);
-			}
-			
-			public String toString() {
-				return filters.toString();
-			}
-		});
-		conf.setRecursive(jsonConf.getBoolean("isRecursive"));
-		conf.setRankDir(jsonConf.getString("rankDir"));
+		if (jsonConf.has("isRecursive"))
+			conf.setRecursive(jsonConf.getBoolean("isRecursive"));
+		
+		if (jsonConf.has("rankDir"))
+			conf.setRankDir(jsonConf.getString("rankDir"));
 
 		return conf;
 	}
 
+	public String getString(String name, JSONObject jsonConf) {
+		if (jsonConf.has(name))
+			return jsonConf.getString(name);
+		
+		return null;
+	}
 }
