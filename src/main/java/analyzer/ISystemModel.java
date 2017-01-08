1a57082a1e796a19ff041abb2815461578be809f
Qinmao Zhang <zhangq2@rose-hulman.edu>
2017-01-08 13:40:57 -0500

    use IExpander in ISystemModel

--- src/main/java/analyzer/ISystemModel.java
+++ src/main/java/analyzer/ISystemModel.java
@@ -2,6 +2,8 @@ package analyzer;
 
 import generator.IGraph;
 import generator.IVertex;
+import utility.IExpander;
+import utility.IMapper;
 
 import java.util.*;
 
@@ -11,81 +13,41 @@ import java.util.*;
  * Created by lamd on 12/9/2016.
  */
 public interface ISystemModel extends IGraph {
-    /**
-     * Returns an Iterable of Class Models contained within the System Model.
-     *
-     * @return Iterable of Class Models.
-     */
-    Collection<? extends IClassModel> getClasses();
+	/**
+	 * Returns an Iterable of Class Models contained within the System Model.
+	 *
+	 * @return Iterable of Class Models.
+	 */
+	Collection<? extends IClassModel> getClasses();
 
-    /**
-     * Returns an Iterable of Relations contained within the SystemModel.
-     *
-     * @return Iterable of Relations contained within the SystemModel.
-     */
-    Map<ClassPair, List<IRelationInfo>> getRelations();
+	/**
+	 * Returns an Iterable of Relations contained within the SystemModel.
+	 *
+	 * @return Iterable of Relations contained within the SystemModel.
+	 */
+	Map<ClassPair, List<IRelationInfo>> getRelations();
 
-    /**
-     * Returns the vertices of the System Model. Class getClasses.
-     *
-     * @return Returns the classes of the System Model.
-     */
-    default Iterable<? extends IVertex> getVertices() {
-        return getClasses();
-    }
+	/**
+	 * Returns the vertices of the System Model. Class getClasses.
+	 *
+	 * @return Returns the classes of the System Model.
+	 */
+	default Iterable<? extends IVertex> getVertices() {
+		return getClasses();
+	}
 
-    /**
-     * Returns the Iterable of Relation edges.
-     *
-     * @return Iterable of Relation edges.
-     */
-    default Iterable<Relation> getEdges() {
-        return () -> new EdgesIterator(getRelations());
-    }
+	/**
+	 * Returns the Iterable of Relation edges.
+	 *
+	 * @return Iterable of Relation edges.
+	 */
+	default Iterable<Relation> getEdges() {
+		Map<ClassPair, List<IRelationInfo>> relations = getRelations();
+		IExpander<ClassPair, Relation> expander = (key) -> {
+			IMapper<IRelationInfo, Relation> mapper = (info) -> new Relation(key, info);
+			return mapper.map(relations.get(key));
+		};
+		return expander.expand(relations.keySet());
+	}
 
-    class EdgesIterator implements Iterator<Relation> {
-        private Map<ClassPair, ? extends Iterable<IRelationInfo>> relationMap;
-        private Relation cur, next, c;
-        private Iterator<ClassPair> keyIterator;
-        private ClassPair curClassPair;
-        private Iterator<IRelationInfo> valueIterator;
-        private boolean hasNext;
-
-        EdgesIterator(Map<ClassPair, ? extends Iterable<IRelationInfo>> map) {
-            relationMap = map;
-            cur = new Relation();
-            next = new Relation();
-            keyIterator = map.keySet().iterator();
-            valueIterator = Collections.EMPTY_LIST.iterator();
-            hasNext = advance();
-        }
-
-        private boolean advance() {
-            this.c = this.cur;
-            this.cur = this.next;
-            this.next = this.c;
-
-            while (!this.valueIterator.hasNext()) {
-                if (!this.keyIterator.hasNext()) {
-                    return false;
-                }
-                this.curClassPair = this.keyIterator.next();
-                this.valueIterator = this.relationMap.get(curClassPair).iterator();
-            }
-
-            this.next.set(curClassPair, valueIterator.next());
-            return true;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return hasNext;
-        }
-
-        @Override
-        public Relation next() {
-            hasNext = advance();
-            return cur;
-        }
-    }
 }
