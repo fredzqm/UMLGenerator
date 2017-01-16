201d4304862489072b0fdc4e1060ccb33743953a
David Lam <dav_lam@ymail.com>
2017-01-15 21:29:24 -0500

    added test

---
+++ src/test/java/analyzer/favorComposition/FavorCompositionAnalyzerTest.java
@@ -0,0 +1,73 @@
+package analyzer.favorComposition;
+
+import analyzer.utility.IAnalyzer;
+import analyzer.utility.IClassModel;
+import analyzer.utility.ISystemModel;
+import org.junit.Test;
+import utility.ClassType;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by lamd on 1/15/2017.
+ */
+public class FavorCompositionAnalyzerTest {
+    @Test
+    public void analyze() throws Exception {
+        // mock creation
+        String noComposition = "ClassA";
+        String composition = "ClassB";
+        String concreteSuperClass = "ConcreteClass";
+        String objectClass = "java.lang.Object";
+
+        IClassModel objectClassModel = mock(IClassModel.class, objectClass);
+        when(objectClassModel.getName()).thenReturn(objectClass);
+
+        IClassModel concreteClassModel = mock(IClassModel.class, concreteSuperClass);
+        when(concreteClassModel.getName()).thenReturn(concreteSuperClass);
+        when(concreteClassModel.getType()).thenReturn(ClassType.CONCRETE);
+
+        IClassModel noCompositionClassModel = mock(IClassModel.class, noComposition);
+        when(noCompositionClassModel.getName()).thenReturn(noComposition);
+        when(noCompositionClassModel.getSuperClass()).thenReturn(concreteClassModel);
+        when(noCompositionClassModel.getNodeStyle()).thenReturn("");
+
+        IClassModel compositionClassModel = mock(IClassModel.class, composition);
+        when(compositionClassModel.getName()).thenReturn(composition);
+        when(compositionClassModel.getSuperClass()).thenReturn(objectClassModel);
+        when(compositionClassModel.getNodeStyle()).thenReturn("");
+
+        // create systemModel
+        ISystemModel systemModelMock = mock(ISystemModel.class);
+        doReturn(Arrays.asList(compositionClassModel, noCompositionClassModel)).when(systemModelMock).getClasses();
+
+        // start
+        ISystemModel systemModel = runAnalyzer(systemModelMock);
+
+        // get decorated classModels
+        Collection<? extends IClassModel> classList = systemModel.getClasses();
+        assertEquals(2, classList.size());
+
+        Iterator<? extends IClassModel> classIterator = classList.iterator();
+        IClassModel favor = classIterator.next();
+        IClassModel noFavor = classIterator.next();
+
+        assertEquals("This should not have a node style as it does not break the principle: Favor Composition over Inheritance.", "", favor.getNodeStyle());
+        assertEquals("This should have a node style as it does break the principle: Favor Composition over Inheritance.", "color=\"orange\" ", noFavor.getNodeStyle());
+        
+
+        // verify
+        verify(noCompositionClassModel).getSuperClass();
+    }
+
+    private ISystemModel runAnalyzer(ISystemModel systemModelMock) {
+        IAnalyzer analyzer = new FavorCompositionAnalyzer();
+        return analyzer.analyze(systemModelMock, null);
+    }
+
+}
