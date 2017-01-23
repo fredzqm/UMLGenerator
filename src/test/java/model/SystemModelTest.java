fbbf1ab7d4769bccf8367c0aec8035e05c956e49
David Lam <dav_lam@ymail.com>
2017-01-22 21:37:52 -0500

    refactored test to rely on mocks

--- src/test/java/model/SystemModelTest.java
+++ src/test/java/model/SystemModelTest.java
@@ -9,36 +9,19 @@ import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SystemModelTest {
     @Test
     public void testLabelAndPrototype() {
-        IModelConfiguration config = new IModelConfiguration() {
-            @Override
-            public boolean isRecursive() {
-                return false;
-            }
+        IModelConfiguration config = mock(IModelConfiguration.class);
+        when(config.isRecursive()).thenReturn(false);
+        when(config.getBlackList()).thenReturn(null);
+        when(config.isVerbose()).thenReturn(false);
+        when(config.filterSynthetic()).thenReturn(false);
+        when(config.getClasses()).thenReturn(Collections.singletonList("javax.swing.JComponent"));
 
-            @Override
-            public Iterable<String> getBlackList() {
-                return null;
-            }
-
-            @Override
-            public boolean isVerbose() {
-                return false;
-            }
-
-            @Override
-            public boolean filterSynthetic() {
-                return false;
-            }
-
-            @Override
-            public Iterable<String> getClasses() {
-                return Collections.singletonList("javax.swing.JComponent");
-            }
-        };
         SystemModel sys = SystemModel.getInstance(config);
 
         // rels
@@ -52,32 +35,13 @@ public class SystemModelTest {
 
     @Test
     public void recursiveParsing() {
-        IModelConfiguration config = new IModelConfiguration() {
-            @Override
-            public boolean isRecursive() {
-                return true;
-            }
+        IModelConfiguration config = mock(IModelConfiguration.class);
+        when(config.isRecursive()).thenReturn(true);
+        when(config.getBlackList()).thenReturn(null);
+        when(config.isVerbose()).thenReturn(false);
+        when(config.filterSynthetic()).thenReturn(false);
+        when(config.getClasses()).thenReturn(Collections.singletonList("javax.swing.JComponent"));
 
-            @Override
-            public Iterable<String> getBlackList() {
-                return null;
-            }
-
-            @Override
-            public boolean isVerbose() {
-                return false;
-            }
-
-            @Override
-            public boolean filterSynthetic() {
-                return true;
-            }
-
-            @Override
-            public Iterable<String> getClasses() {
-                return Collections.singletonList("javax.swing.JComponent");
-            }
-        };
         SystemModel sys = SystemModel.getInstance(config);
 
         Set<String> actual = new HashSet<>();
@@ -85,8 +49,9 @@ public class SystemModelTest {
                 "java.awt.Component", "java.lang.Object", "java.awt.image.ImageObserver", "java.awt.MenuContainer",
                 "java.io.Serializable", "javax.swing.TransferHandler$HasGetTransferHandler"));
 
-        for (IClassModel x : sys.getClasses())
+        for (IClassModel x : sys.getClasses()) {
             actual.add(x.getName());
+        }
 
         assertTrue("Not all interfaces get parsed", actual.containsAll(expected));
     }
