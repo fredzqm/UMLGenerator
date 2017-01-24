22895e9c79a908ec98266caa3a4cf823679ddec2
Qinmao Zhang <zhangq2@rose-hulman.edu>
2017-01-23 23:31:12 -0500

    make FavorCompositionSystemModel extends ISystemModelFilter and take advantage of the proper decorator pattern

--- src/main/java/analyzer/favorComposition/FavorCompositionSystemModel.java
+++ src/main/java/analyzer/favorComposition/FavorCompositionSystemModel.java
@@ -1,33 +1,40 @@
 package analyzer.favorComposition;
 
-import analyzer.utility.ClassPair;
-import analyzer.utility.IClassModel;
-import analyzer.utility.IRelationInfo;
-import analyzer.utility.ISystemModel;
-
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+
+import analyzer.utility.IClassModel;
+import analyzer.utility.ISystemModel;
+import analyzer.utility.ISystemModelFilter;
+import utility.ClassType;
 
 /**
  * Created by lamd on 1/15/2017.
  */
-public class FavorCompositionSystemModel implements ISystemModel {
-    private ISystemModel systemModel;
-    private Collection<? extends IClassModel> classes;
+public class FavorCompositionSystemModel extends ISystemModelFilter {
+    private FavorCompositionConfiguration config;
 
-    FavorCompositionSystemModel(ISystemModel systemModel, Collection<? extends IClassModel> classes) {
-        this.systemModel = systemModel;
-        this.classes = classes;
+    FavorCompositionSystemModel(ISystemModel systemModel, FavorCompositionConfiguration favorComConfig) {
+        super(systemModel);
+        this.config = favorComConfig;
     }
 
     @Override
     public Collection<? extends IClassModel> getClasses() {
-        return this.classes;
+        Collection<IClassModel> analyzedClass = new ArrayList<>();
+        super.getClasses().forEach((c) -> {
+            if (violateFavorComposition(c)) {
+                analyzedClass.add(new FavorCompositionClassModel(c, config));
+            } else {
+                analyzedClass.add(c);
+            }
+        });
+        return analyzedClass;
     }
 
-    @Override
-    public Map<ClassPair, List<IRelationInfo>> getRelations() {
-        return this.systemModel.getRelations();
+    private boolean violateFavorComposition(IClassModel clazz) {
+        IClassModel superClass = clazz.getSuperClass();
+        return superClass.getType() == ClassType.CONCRETE && !superClass.getName().equals("java.lang.Object");
     }
+
 }
