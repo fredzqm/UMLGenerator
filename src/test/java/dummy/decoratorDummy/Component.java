6320d4d81bde25082cb8fd75e9f293a8e1263b44
David Lam <dav_lam@ymail.com>
2017-02-05 01:44:17 -0500

    adding some dummy test class for debugging

--- src/test/java/dummy/decoratorDummy/Component.java
+++ src/test/java/dummy/decoratorDummy/Component.java
@@ -4,4 +4,7 @@ package dummy.decoratorDummy;
  * Created by lamd on 2/4/2017.
  */
 public class Component {
+    public String createString() {
+        return "component";
+    }
 }
--- src/test/java/dummy/decoratorDummy/Decorator.java
+++ src/test/java/dummy/decoratorDummy/Decorator.java
@@ -3,5 +3,15 @@ package dummy.decoratorDummy;
 /**
  * Created by lamd on 2/4/2017.
  */
-public class Decorator {
+public abstract class Decorator extends Component {
+    private Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String createString() {
+        return super.createString();
+    }
 }
