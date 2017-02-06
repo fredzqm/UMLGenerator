354cd5d68093fc2bd2003a3fa693697534510b9f
Qinmao Zhang <zhangq2@rose-hulman.edu>
2017-02-06 16:48:30 -0500

    optmize

--- src/main/java/analyzer/utility/StyleMap.java
+++ src/main/java/analyzer/utility/StyleMap.java
@@ -11,6 +11,10 @@ public class StyleMap {
         style = String.format("%s %s=\"%s\"", style, key, value);
     }
 
+    public void addStyleMap(StyleMap styleMap) {
+        style = style + " " + styleMap.getStyleString();
+    }
+
     public String getStyleString() {
         return style;
     }
