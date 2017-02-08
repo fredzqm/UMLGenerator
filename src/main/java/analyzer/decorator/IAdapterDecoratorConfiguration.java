aa894fbfe3de62811914e3569f8305390339cda9
David Lam <dav_lam@ymail.com>
2017-02-08 04:30:10 -0500

    added docs IAdapterDecoratorConfiguration

--- src/main/java/analyzer/decorator/IAdapterDecoratorConfiguration.java
+++ src/main/java/analyzer/decorator/IAdapterDecoratorConfiguration.java
@@ -3,14 +3,36 @@ package analyzer.decorator;
 import config.Configurable;
 
 /**
+ * A Common AdapterDecorator Configuration Interface.
+ * <p>
  * Created by lamd on 2/7/2017.
  */
 public interface IAdapterDecoratorConfiguration extends Configurable {
+    /**
+     * Returns the fill color of the Class that meets the pattern analyser.
+     *
+     * @return String of the fill color.
+     */
     String getFillColor();
 
+    /**
+     * Returns the Parent Sterotype label that meets the pattern analyzer class.
+     *
+     * @return String of parent sterotype label.
+     */
     String getParentStereotype();
 
+    /**
+     * Returns the Child Sterotype label that meets the pattern analyzer class.
+     *
+     * @return String of the child stereotype label.
+     */
     String getChildStereotype();
 
+    /**
+     * Returns the ChildParent Relation lable.
+     *
+     * @return String of Relation Label.
+     */
     String getChildParentRelationshipLabel();
 }
